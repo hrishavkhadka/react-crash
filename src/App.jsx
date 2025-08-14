9c10bb9b3fe8a6038b92933f710bcabe165d049f
hrishav <notrisavkhadka@gmail.com>
2025-08-14 17:33:17 +0545

    before active links

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,14 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 import MainLayout from './layouts/MainLayout'
 import HomePage from './pages/HomePage'
 import JobsPage from "./pages/JobsPage";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import ViewAllJobs from './components/ViewAllJobs'
+import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element = {<MainLayout />}>
       <Route index element = {<HomePage />} />
       <Route path='/jobs' element = {<JobsPage />} />
+      <Route path='*' element = {<NotFoundPage />} />
     </Route>
   )
 );
